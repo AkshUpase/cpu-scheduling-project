ed7de65e83a9b15523246efdff808ad9d710d138
AkshUpase <Akshupase06@gmail.com>
2026-03-24 20:20:44 +0530

    Added SJF and Round Robin algorithms

--- cpu-scheduling-project/include/scheduling.h
+++ cpu-scheduling-project/include/scheduling.h
@@ -13,5 +13,7 @@ struct process
 void input(struct process p[], int n);
 void display(struct process p[], int n);
 void fcfs(struct process p[], int n);
+void sjf(struct process[], int n);
+void round_robin(struct process p[], int n, int quantum)
 
 #endif
