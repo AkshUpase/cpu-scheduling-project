ad9bd59c60a02fce41a41892aebe0b1ac22f28fe
AkshUpase <Akshupase06@gmail.com>
2026-03-24 20:32:02 +0530

    Added Round Robin algorithm and updated header

--- cpu-scheduling-project/include/scheduling.h
+++ cpu-scheduling-project/include/scheduling.h
@@ -14,6 +14,6 @@ void input(struct process p[], int n);
 void display(struct process p[], int n);
 void fcfs(struct process p[], int n);
 void sjf(struct process[], int n);
-void round_robin(struct process p[], int n, int quantum)
+void round_robin(struct process p[], int n, int quantum);
 
 #endif
